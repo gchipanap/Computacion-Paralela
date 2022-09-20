c964e444f930e9d5806c03c4d5b442b1d832d7cf
Gabriela <64268942+gchipanap@users.noreply.github.com>
2022-09-20 16:38:38 -0500

    Add files via upload

---
+++ multiplicacionesMatrices.cpp
@@ -0,0 +1,185 @@
+#include <iostream>
+#include <ctime> 
+#include <cmath>
+#include <vector>
+#include <sys/time.h>
+
+void fillMatrix(short **matrix, int size);
+void fillZeroMatrix(short **matrix, int size);
+
+void deleteMatrix(short **matrix, int size);
+void displayMatrix(short **matrix, int size);
+
+void multiplyClassic(short **matrix1, short **matrix2, short **matrix3, int size);
+void multiplyBlock(short **matrix1, short **matrix2, short **matrix3, int size, int block_size);
+
+void timingMultiplyClassic(int size, std::vector<double> & TmultiplyClassic, std::vector<double> & TmultiplyBlock);
+double average(std::vector<double> T, int size);
+
+int main()
+{	
+	for(int i = 2; i < 15; i++)
+	{
+		int size = pow(2,i);
+		
+		std::vector<double> TmultiplyClassic;
+		std::vector<double> TmultiplyBlock;
+		
+		timingMultiplyClassic(size, TmultiplyClassic, TmultiplyBlock);
+		
+		double TmulClassic = average(TmultiplyClassic, 10);
+		double TmulBlock = average(TmultiplyBlock, 10);
+		
+		std::cout << size << "\t" << TmulClassic << "\t" << TmulBlock << "\n";	
+	}
+
+	
+	return 0;
+}
+	
+	
+void fillMatrix(short **matrix, int size)
+{
+	srand(time(NULL));
+	for(int i = 0; i < size; i++)
+	{
+		matrix[i] = new short[size];
+		for(int j = 0; j< size; j++)
+		{
+			matrix[i][j] = std::rand() % 10;
+		}
+	}	
+}
+
+void fillZeroMatrix(short **matrix, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		matrix[i] = new short[size];
+		for (int j = 0; j < size; j++)
+		{
+			matrix[i][j] = 0.0f;
+		}
+	}
+}
+
+void deleteMatrix(short **matrix, int size)
+{
+	for (int i = 0; i < size; i++) 
+	{
+        delete [] matrix[i];
+    }
+    delete [] matrix;
+}
+
+void displayMatrix(short **matrix, int size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		for(int j = 0; j< size; j++)
+			std::cout << matrix[i][j] << "\t";
+		std::cout<<"\n";
+	}	
+}
+
+void multiplyClassic(short **matrix1, short **matrix2, short **matrix3, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			for (int k = 0; k < size; k++)
+			{
+				matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
+			}
+		}
+	}	
+}
+
+void multiplyBlock(short **matrix1, short **matrix2, short **matrix3, int size, int block_size)
+{
+	int en = block_size * (size/block_size); 
+	double sum;
+	
+	for (int kk = 0; kk < en; kk += block_size) 
+	{
+		for (int jj = 0; jj < en; jj += block_size) 
+		{
+			for (int i = 0; i < size; i++) 
+			{
+				for (int j = jj; j < jj + block_size; j++) 
+				{
+					sum = matrix3[i][j];
+					for (int k = kk; k < kk + block_size; k++) 
+					{
+						sum += matrix1[i][k]*matrix2[k][j];
+					}
+					matrix3[i][j] = sum;
+				}
+			}
+		}
+	}
+}	
+
+void timingMultiplyClassic(int size, std::vector<double> & TmultiplyClassic, std::vector<double> & TmultiplyBlock)
+{
+	std::clock_t start, end;
+	//double timeLoop1, timeLoop2;
+	long long timeLoop1, timeLoop2;
+
+	struct timeval time_start;
+	struct timeval time_end;
+
+	for(int i = 0 ; i < 10; i++)
+	{
+		short **matrix1 = new short *[size];
+		short **matrix2 = new short *[size];
+		short **matrix3 = new short *[size];
+		
+		/* Fill matrix */
+		fillMatrix(matrix1, size);
+		fillMatrix(matrix2, size);
+		fillZeroMatrix(matrix3, size);
+		
+		/* Multiply - 2 loops */
+		start = clock();
+		gettimeofday(&time_start, NULL);
+		multiplyClassic(matrix1, matrix2, matrix3, size);
+		gettimeofday(&time_end, NULL);
+		end = clock();
+		
+		timeLoop1 = 1000000LL
+				* (time_end.tv_sec  - time_start.tv_sec)
+				+ (time_end.tv_usec - time_start.tv_usec);
+		
+		/* Fill matrix3 with 0*/
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++)
+				matrix3[i][j] = 0.0;
+		
+		/* Multiply - 6 loops */
+		start = clock();
+		gettimeofday(&time_start, NULL);
+		multiplyBlock(matrix1, matrix2, matrix3, size, size/4);
+		gettimeofday(&time_end, NULL);
+		end = clock();
+		
+		timeLoop2  = 1000000LL
+				* (time_end.tv_sec  - time_start.tv_sec)
+				+ (time_end.tv_usec - time_start.tv_usec);
+		
+		deleteMatrix(matrix1, size);
+		deleteMatrix(matrix2, size);
+		deleteMatrix(matrix3, size);
+		
+ 		TmultiplyClassic.push_back(timeLoop1);
+		TmultiplyBlock.push_back(timeLoop2);
+	}
+}
+double average(std::vector<double> T, int size)
+{
+	int sum;
+	for(int i = 0; i < size; i++)
+		sum += T[i];
+	return sum /size;
+}
